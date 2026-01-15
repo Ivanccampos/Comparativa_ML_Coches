1aeb27e39bed8420e74f6ee285bcc3ee323199ad
Ivanccampos <122456303+Ivanccampos@users.noreply.github.com>
2026-01-15 17:30:24 +0100

    Enhance app with Streamlit and Matplotlib features
    Added Streamlit components and a Matplotlib chart to the app.

--- web_de_prediccion_de_precio_de_coche.py
+++ web_de_prediccion_de_precio_de_coche.py
@@ -1,10 +1,26 @@
 import streamlit as st
-import joblib
+
+st.write("Hello, *World!* :sunglasses:")
+# Draw a title and some text to the app:
+'''
+# This is the document title
+
+This is some _markdown_.
+'''
+
 import pandas as pd
+df = pd.DataFrame({'col1': [1,2,3]})
+df  # 👈 Draw the dataframe
 
-st.set_page_config(page_title="Precio de coche", layout="centered")
+x = 10
+'x', x  # 👈 Draw the string 'x' and then the value of x
 
-st.title("🚗 Predicción del precio de un coche")
-st.header("amongus", divide="rainbow")
+# Also works with most supported chart types
+import matplotlib.pyplot as plt
+import numpy as np
 
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
 
+fig  # 👈 Draw a Matplotlib chart
